41ddb716ddf427b374e6f44559f077cd560e251a
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2023-11-01 20:46:53 +0100

    ++++

---
+++ test/num.ts
@@ -0,0 +1,41 @@
+import parse from '../index.js'
+import { test } from 'node:test'
+import assert from 'node:assert/strict'
+
+test('nums', () => {
+  const argv = parse([
+    '-x',
+    '1234',
+    '-y',
+    '5.67',
+    '-z',
+    '1e7',
+    '-w',
+    '10f',
+    '--hex',
+    '0xdeadbeef',
+    '789',
+  ])
+  assert.deepEqual(argv, {
+    x: 1234,
+    y: 5.67,
+    z: 1e7,
+    w: '10f',
+    hex: 0xdeadbeef,
+    _: [789],
+  })
+  assert.equal(typeof argv.x, 'number')
+  assert.equal(typeof argv.y, 'number')
+  assert.equal(typeof argv.z, 'number')
+  assert.equal(typeof argv.w, 'string')
+  assert.equal(typeof argv.hex, 'number')
+  assert.equal(typeof argv._[0], 'number')
+})
+
+test('already a number', () => {
+  // @ts-expect-error - wrong argument types
+  const argv = parse(['-x', 1234, 789])
+  assert.deepEqual(argv, { x: 1234, _: [789] })
+  assert.equal(typeof argv.x, 'number')
+  assert.equal(typeof argv._[0], 'number')
+})
